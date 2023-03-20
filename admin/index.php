c8aaacce0df1b5ad13dae0d2084f349306878dab
Andrew Bish <aebish@mail.fhsu.edu>
2023-03-19 20:54:03 -0700

    fixing other issues with css and other connections

--- admin/index.php
+++ admin/index.php
@@ -63,13 +63,13 @@ if($makeId && $typeId && $classId && $radiofilter) {
     include('view/admin_vehicles.php');
 } elseif($addMake) { 
     addMake($addMake);
-    header('Location: http://localhost/aebMidTermProj/admin/?action=displayMake');
+    header('Location: http://aebmidterm.herokuapp.com/admin/?action=displayMake');
 } elseif($addTypes) {
     addTypes($addTypes);
-    header('Location: http://localhost/aebMidTermProj/admin/?action=displayType');
+    header('Location: http://aebmidterm.herokuapp.com/admin/?action=displayType');
 } elseif($addClass) {
     addClass($addClass);
-    header('Location: http://localhost/aebMidTermProj/admin/?action=displayClass');
+    header('Location: http://aebmidterm.herokuapp.com/admin/?action=displayClass');
 } elseif($year && $model && $price && $typeID && $classID && $makeID) {
     addVehichle($year, $model, $price, $typeID, $classID, $makeID);
     header('Location: http://localhost/aebMidTermProj/admin/');
@@ -88,21 +88,21 @@ if($makeId && $typeId && $classId && $radiofilter) {
             break;
         case "deleteMake" :
             deleteMake($makeIdNum);
-            header('Location: http://localhost/aebMidTermProj/admin/?action=displayMake');
+            header('Location: http://aebmidterm.herokuapp.com/admin/?action=displayMake');
         case "displayMake" :
             $makes = displayMakes();
             include('view/displayMake.php');
             break;
         case "deleteTypes" :
             deleteTypes($typeIdNum);
-            header('Location: http://localhost/aebMidTermProj/admin/?action=displayType');
+            header('Location: http://aebmidterm.herokuapp.com/admin/?action=displayType');
         case "displayType" :
             $types = displayTypes();
             include('view/admin_types.php');
             break;
         case "deleteClass" :
             deleteClass($classIdNum);
-            header('Location: http://localhost/aebMidTermProj/admin/?action=displayClass');
+            header('Location: http://aebmidterm.herokuapp.com/admin/?action=displayClass');
         case "displayClass" :
             $classes = displayClasses();
             include('view/admin_classes.php');
